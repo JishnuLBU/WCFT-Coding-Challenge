25c0728eccebca5d3896851a70dce7836b9d4eb6
JishnuLBU <jishnu35@gmail.com>
2024-05-16 07:42:35 +0100

    Added correctionon database file

--- database.sql
+++ database.sql
@@ -1,33 +1,78 @@
 CREATE DATABASE WCFTCodeChallangeDB
 GO
 
-CREATE TABLE patients (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    first_name NVARCHAR(100) NOT NULL,
-    surname NVARCHAR(100) NOT NULL,
-    date_of_birth DATE NOT NULL,
-    age INT NOT NULL
-);
+/****** Object:  Table [dbo].[patients]    Script Date: 5/16/2024 7:29:59 AM ******/
 
+USE WCFTCodeChallangeDB
+SET ANSI_NULLS ON
 GO
 
-CREATE TABLE questions (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    panel NVARCHAR(50) NOT NULL,
-    question_text NVARCHAR(MAX) NOT NULL,
-    max_score INT NOT NULL
-);
-
+SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE patient_responses (
-    id INT IDENTITY(1,1) PRIMARY KEY,
-    patient_id INT NOT NULL,
-    question_id INT NOT NULL,
-    score INT NOT NULL,
-    FOREIGN KEY (patient_id) REFERENCES patients(id),
-    FOREIGN KEY (question_id) REFERENCES questions(id)
-);
+CREATE TABLE [dbo].[patients](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[first_name] [nvarchar](100) NOT NULL,
+	[surname] [nvarchar](100) NOT NULL,
+	[date_of_birth] [date] NOT NULL,
+	[age] [int] NOT NULL,
+	[total_score] [int] NULL,
+	[created_at] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[patients] ADD  DEFAULT (getdate()) FOR [created_at]
+GO
+
+
+/****** Object:  Table [dbo].[questions]    Script Date: 5/16/2024 7:30:46 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[questions](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[panel] [nvarchar](50) NOT NULL,
+	[question_text] [nvarchar](max) NOT NULL,
+	[max_score] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+/****** Object:  Table [dbo].[patient_responses]    Script Date: 5/16/2024 7:28:54 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[patient_responses](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[patient_id] [int] NOT NULL,
+	[question_id] [int] NOT NULL,
+	[score] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[patient_responses]  WITH CHECK ADD FOREIGN KEY([patient_id])
+REFERENCES [dbo].[patients] ([id])
+GO
+
+ALTER TABLE [dbo].[patient_responses]  WITH CHECK ADD FOREIGN KEY([question_id])
+REFERENCES [dbo].[questions] ([id])
 GO
 
 INSERT INTO questions (panel, question_text, max_score) VALUES
@@ -45,3 +90,11 @@ INSERT INTO questions (panel, question_text, max_score) VALUES
 ('Brief Pain Inventory (BPI)', 'Based on the number that best describes how during the past week pain has INTERFERED with your: Enjoyment of life.', 10);
 
 GO
+CREATE TABLE users (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(250) NOT NULL
+);
+
+INSERT INTO users(username, password) VALUES ('admin@test.com', '$2y$10$vQJhUkeCweAq41LT7PBLTOOFb4pQl8XLXIf6VApZjQWJ4PeUlSZeG'
+$2y$10$6xh12rAtt9S.F7ujIywAiO9kak/bnhN8b1vWN/fz6OiAj/95/Fh12);
